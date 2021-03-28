a217d3d1742464f33c049c8d116d3d25d37abc29
ionut270 <oanceaionut15@gmail.com>
2021-03-28 14:20:15 +0300

    Added dotenv

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 'use strict';
 console.out = (str) => console.log(`${Date.now()} : ${str}`)
 
+require("dotenv").config();
 const express   = require("express");
 const bodyParser= require("body-parser");
 const cors      = require("cors");
