844462ac2d4a6434f7c76580568e6622472bee0b
Marius Dinu <dinumariusciprian@gmail.com>
2021-03-29 20:09:25 +0300

    add

--- server/router/ui.js
+++ server/router/ui.js
@@ -1,6 +1,7 @@
 const utils = require('../utils/utils');
 const path = require('path');
 const fs = require('fs');
+const directions = require('../utils/directions');
 
 module.exports = (app) => {
     app.get(['/', '/login'], (req, res) => {
@@ -9,8 +10,11 @@ module.exports = (app) => {
     app.get(['/profile'], utils.ensureAuthenticated, (req, res) => {
         res.sendFile(path.join(__dirname + '/../../client/build/index.html'));
     })
-    app.get(['/map'], utils.ensureAuthenticated, (req, res) => {
+    app.get(['/map'], (req, res) => {
         res.sendFile(path.join(__dirname + '/../../client/build/location.html'));
+    })
+    app.get(['/directions'], async(req, res) => {
+        res.send(await directions.findDirections(req.query.id1, req.query.id2));
     })
 
 }
